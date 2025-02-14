8a887f4f342346a8a1dcfcff8117e42a6876072b
laurazup <laura.rodrigues@zup.com.br>
2025-02-14 14:36:59 -0300

    feat: adiciona entidade cliente na model

---
+++ src/main/java/com/crud/crudClientes/models/Client.java
@@ -0,0 +1,19 @@
+package com.crud.crudClientes.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    private String email;
+
+    @Column(name = "telefone")
+    private String phone;
+}
