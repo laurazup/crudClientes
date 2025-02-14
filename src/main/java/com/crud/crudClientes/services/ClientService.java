080cf593c6f5b422b5134620e135661b8fe30ef3
laurazup <laura.rodrigues@zup.com.br>
2025-02-14 15:00:01 -0300

    feat: adiciona ClientService com atributo repository e construtor

--- src/main/java/com/crud/crudClientes/services/ClientService.java
+++ src/main/java/com/crud/crudClientes/services/ClientService.java
@@ -1,4 +1,14 @@
 package com.crud.crudClientes.services;
 
+import com.crud.crudClientes.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClientService {
+    private final ClientRepository clientRepository;
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
 }
