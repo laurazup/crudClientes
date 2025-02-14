35f226fd6379ebea929b7cce47fdebf00e576a02
laurazup <laura.rodrigues@zup.com.br>
2025-02-14 15:20:51 -0300

    refactor: muda nome da classe ClientService para ClientServiceImpl e cria interface ClientService

--- src/main/java/com/crud/crudClientes/services/ClientService.java
+++ src/main/java/com/crud/crudClientes/services/ClientService.java
@@ -1,20 +1,9 @@
 package com.crud.crudClientes.services;
 
 import com.crud.crudClientes.models.ClientModel;
-import com.crud.crudClientes.repositories.ClientRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class ClientService {
-    private final ClientRepository clientRepository;
-    public ClientService(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
-    }
-
-    public List<ClientModel> getAllClients() {
-        List<ClientModel> clients = clientRepository.findAll();
-        return clients;
-    }
+public interface ClientService {
+    List<ClientModel> getAllClients();
 }
