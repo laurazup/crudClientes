38e3f9ab9e744658066150c608e8a058a2cb5640
laurazup <laura.rodrigues@zup.com.br>
2025-02-17 16:43:40 -0300

    feat: adiciona spring security

---
+++ src/main/java/com/crud/crudClientes/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.crud.crudClientes.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/clients").permitAll() // Permite acesso público ao endpoint /api/clients
+                        .anyRequest().authenticated() // Exige autenticação para qualquer outra rota
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // Página de login personalizada
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Criptografa senhas
+    }
+}
